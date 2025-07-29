0771f040414f9fca91d791bc170d4c3f542538e4
opixelum <48217573+opixelum@users.noreply.github.com>
2025-07-29 16:07:49 -0400

    feat(backend): get api port from env

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,13 +2,21 @@ mod api;
 mod db;
 
 use axum::{Error, Router};
+use dotenvy::dotenv;
+use std::env;
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    dotenv().ok();
+
+    let api_port: u16 =
+        str::parse(&env::var("API_PORT").expect("Undefined API_PORT environment variable"))
+            .unwrap();
+
     let app: Router = api::app::router().await;
 
-    let addr: SocketAddr = "0.0.0.0:8080".parse().expect("");
+    let addr: SocketAddr = format!("0.0.0.0:{api_port}").parse().expect("");
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     println!("Listening on {addr}");
 
