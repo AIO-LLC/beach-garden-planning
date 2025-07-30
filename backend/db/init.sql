382eb722c995293d2085c4a15e908324b4d569c2
opixelum <48217573+opixelum@users.noreply.github.com>
2025-07-30 17:52:53 -0400

    feat(db): add members table

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -1,6 +1,11 @@
-CREATE TABLE events (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(64) NOT NULL,
-  date DATE NOT NULL,
-  time TIME NOT NULL
+CREATE TABLE members (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  email VARCHAR(255) NOT NULL UNIQUE,
+  first_name VARCHAR(64) NOT NULL,
+  last_name VARCHAR(64) NOT NULL,
+  gender CHAR(1) NOT NULL,
+  birth_date DATE NOT NULL,
+  phone VARCHAR(15) NOT NULL,
+  fft_license CHAR(8)
+  signup_date DATE
 );
