20ca68505e0b8a0416344a552d0c5e60102e6bac
opixelum <48217573+opixelum@users.noreply.github.com>
2025-07-31 11:28:33 -0400

    feat(backend): add members queries wrappers for http requests

--- backend/src/api/wrappers.rs
+++ backend/src/api/wrappers.rs
@@ -1,44 +1,65 @@
 use crate::api::app::{ApiError, AppState};
-use crate::db::{models, queries};
+use crate::db::models::Member;
+use crate::db::queries;
 use axum::{
     extract::{Json, Path, State},
     http::StatusCode,
 };
-use chrono::{NaiveDate, NaiveTime};
+use chrono::NaiveDate;
 use serde::Deserialize;
+use uuid::Uuid;
 
 #[derive(Deserialize)]
-pub struct NewEvent {
-    name: String,
-    date: NaiveDate,
-    time: NaiveTime,
+pub struct NewMember {
+    pub first_name: String,
+    pub last_name: String,
+    pub gender: String,
+    pub birth_date: NaiveDate,
+    pub email: String,
+    pub phone: String,
+    pub fft_license: String,
+    pub profile_picture: String,
 }
 
-pub async fn insert_into_events(
+pub async fn add_member(
     State(state): State<AppState>,
-    Json(payload): Json<NewEvent>,
-) -> Result<Json<i32>, ApiError> {
+    Json(payload): Json<NewMember>,
+) -> Result<Json<Uuid>, ApiError> {
     let client = state.pool.get().await?;
-    let id =
-        queries::insert_into_events(&client, &payload.name, payload.date, payload.time).await?;
+    let id = queries::add_member(
+        &client,
+        &Member {
+            id: None,
+            first_name: payload.first_name,
+            last_name: payload.last_name,
+            gender: payload.gender,
+            birth_date: payload.birth_date,
+            email: payload.email,
+            phone: payload.phone,
+            fft_license: Some(payload.fft_license),
+            profile_picture: Some(payload.profile_picture),
+            signup_date: None,
+        },
+    )
+    .await?;
     Ok(Json(id))
 }
 
-pub async fn get_day_events(
+pub async fn get_member(
     State(state): State<AppState>,
-    Path(data): Path<NaiveDate>,
-) -> Result<Json<Vec<models::Event>>, ApiError> {
+    Path(data): Path<Uuid>,
+) -> Result<Json<Member>, ApiError> {
     let client = state.pool.get().await?;
-    let events = queries::get_day_events(&client, data).await?;
-    Ok(Json(events))
+    let member = queries::get_member(&client, data).await?;
+    Ok(Json(member))
 }
 
-pub async fn delete_event(
+pub async fn delete_member(
     State(state): State<AppState>,
-    Path(data): Path<i32>,
+    Path(data): Path<Uuid>,
 ) -> Result<StatusCode, ApiError> {
     let client = state.pool.get().await?;
-    let affected = queries::delete_event(&client, data).await?;
+    let affected = queries::delete_member(&client, data).await?;
     if affected == 1 {
         Ok(StatusCode::NO_CONTENT)
     } else {
