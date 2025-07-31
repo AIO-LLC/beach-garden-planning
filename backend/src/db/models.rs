42e3e0823410a364d29f6f31a901566fd685e045
opixelum <48217573+opixelum@users.noreply.github.com>
2025-07-31 11:25:53 -0400

    feat(backend): add member & address struct

--- backend/src/db/models.rs
+++ backend/src/db/models.rs
@@ -1,10 +1,27 @@
-use chrono::{NaiveDate, NaiveTime};
+use chrono::NaiveDate;
 use serde::Serialize;
+use uuid::Uuid;
 
 #[derive(Debug, Serialize)]
-pub struct Event {
-    pub id: i32,
-    pub name: String,
-    pub date: NaiveDate,
-    pub time: NaiveTime,
+pub struct Member {
+    pub id: Option<Uuid>,
+    pub first_name: String,
+    pub last_name: String,
+    pub gender: String,
+    pub birth_date: NaiveDate,
+    pub email: String,
+    pub phone: String,
+    pub fft_license: Option<String>,
+    pub profile_picture: Option<String>,
+    pub signup_date: Option<NaiveDate>,
 }
+
+//pub struct Address {
+//    pub id: Option<Uuid>,
+//    pub member_id: Option<Uuid>,
+//    pub line_1: String,
+//    pub line_2: Option<String>,
+//    pub postal_code: String,
+//    pub city: String,
+//    pub country: String,
+//}
