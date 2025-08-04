ce90787d7c37785f108dc9c99a7d4092e28da294
opixelum <48217573+opixelum@users.noreply.github.com>
2025-08-04 16:38:33 -0400

    fix(wrappers): return error 404 when no reservation to member relationship

--- backend/src/api/wrappers/reservation_to_member.rs
+++ backend/src/api/wrappers/reservation_to_member.rs
@@ -1,9 +1,6 @@
 use crate::api::app::{ApiError, AppState};
 use crate::db::queries::reservation_to_member;
-use axum::{
-    extract::{Json, Path, State},
-    http::StatusCode,
-};
+use axum::extract::{Json, Path, State};
 use serde::Deserialize;
 use uuid::Uuid;
 
@@ -13,29 +10,19 @@ pub struct ReservationToMember {
     pub member_id: String,
 }
 
-pub async fn add_reservation_to_member(
-    State(state): State<AppState>,
-    Json(payload): Json<ReservationToMember>,
-) -> Result<StatusCode, ApiError> {
-    let client = state.pool.get().await?;
-    reservation_to_member::add_reservation_to_member(
-        &client,
-        &Uuid::parse_str(&payload.reservation_id).expect("Couldn not parse UUID from string."),
-        &Uuid::parse_str(&payload.member_id).expect("Couldn not parse UUID from string."),
-    )
-    .await?;
-    Ok(StatusCode::OK)
-}
-
 pub async fn get_members_from_reservation(
     State(state): State<AppState>,
     Path(reservation_id): Path<Uuid>,
 ) -> Result<Json<Vec<Uuid>>, ApiError> {
     let client = state.pool.get().await?;
-    match reservation_to_member::get_members_from_reservation(&client, &reservation_id).await {
-        Ok(members_ids) => Ok(Json(members_ids)),
-        Err(_) => Err(ApiError::NotFound),
+    let members_ids: Vec<Uuid> =
+        reservation_to_member::get_members_from_reservation(&client, &reservation_id).await?;
+
+    if members_ids.is_empty() {
+        return Err(ApiError::NotFound);
     }
+
+    Ok(Json(members_ids))
 }
 
 pub async fn get_reservations_from_member(
@@ -43,8 +30,12 @@ pub async fn get_reservations_from_member(
     Path(member_id): Path<Uuid>,
 ) -> Result<Json<Vec<Uuid>>, ApiError> {
     let client = state.pool.get().await?;
-    match reservation_to_member::get_reservations_from_member(&client, &member_id).await {
-        Ok(reservations_ids) => Ok(Json(reservations_ids)),
-        Err(_) => Err(ApiError::NotFound),
+    let reservations_ids: Vec<Uuid> =
+        reservation_to_member::get_reservations_from_member(&client, &member_id).await?;
+
+    if reservations_ids.is_empty() {
+        return Err(ApiError::NotFound);
     }
+
+    Ok(Json(reservations_ids))
 }
