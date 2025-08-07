a03f2fb34d2027ccce3b69ba4b1a76bf83078e94
opixelum <48217573+opixelum@users.noreply.github.com>
2025-08-07 01:03:04 -0400

    feat(login): add cookie after log in attempt

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -2,24 +2,30 @@
 
 import React from "react"
 import { Form, Input, Button } from "@heroui/react"
-
+import { useRouter } from 'next/navigation'
+import Cookies from 'js-cookie'
 import { title } from "@/components/primitives"
 
 export default function LogInPage() {
+  const router = useRouter()
+
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
     var payload = Object.fromEntries(new FormData(e.currentTarget))
 
     try {
-      const url = `${API_HOST}:${API_PORT}/login`
-      const response = await fetch(url, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(payload)
-      })
+      // const url = `${API_HOST}:${API_PORT}/login`
+      // const response = await fetch(url, {
+      //   method: "POST",
+      //   headers: { "Content-Type": "application/json" },
+      //   body: JSON.stringify(payload)
+      // })
+      //
+      // if (!response.ok) throw new Error(`Erreur ${response.status}`)
 
-      if (!response.ok) throw new Error(`Erreur ${response.status}`)
+      Cookies.set('loginAttempt', 'true', { expires: 0.01, sameSite: 'lax' })
+      router.push('/confirm')
     } catch (err: any) {
       console.error(err)
     }
