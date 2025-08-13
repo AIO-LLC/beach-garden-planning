46d6f7c8739d2970573b86ac343f3e645f22b156
opixelum <48217573+opixelum@users.noreply.github.com>
2025-08-13 17:58:46 -0400

    feat(frontend): add password forgotten page

---
+++ frontend/app/password-forgotten/page.tsx
@@ -0,0 +1,102 @@
+"use client"
+
+import React from "react"
+import { Form, Input, Button, addToast } from "@heroui/react"
+import * as EmailValidator from "email-validator"
+import { title } from "@/components/primitives"
+
+const API_HOST = process.env.NEXT_PUBLIC_API_HOST!
+const API_PORT = process.env.NEXT_PUBLIC_API_PORT!
+
+export default function PasswordForgottenPage() {
+  const [email, setEmail] = React.useState("")
+
+  const getEmailError = (value: string): string | null => {
+    if (value.length === 0) return null
+
+    if (!EmailValidator.validate(value)) {
+      return "L'adresse email est invalide."
+    }
+
+    return null
+  }
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    try {
+      const getJwtClaimsResponse = await fetch(
+        `${API_HOST}:${API_PORT}/jwt-claims`,
+        {
+          method: "GET",
+          credentials: "include"
+        }
+      )
+
+      if (!getJwtClaimsResponse.ok) {
+        addToast({
+          title: "Une erreur est survenue. Veuillez réessayer plus tard.",
+          color: "danger"
+        })
+        const { error } = await getJwtClaimsResponse.json()
+        console.error(error)
+        return
+      }
+
+      const { id, _phone, _isProfileComplete } =
+        await getJwtClaimsResponse.json()
+
+      const payload = { id, email }
+
+      const url = `${API_HOST}:${API_PORT}/password-forgotten`
+      const passwordForgottenResponse = await fetch(url, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+        body: JSON.stringify(payload)
+      })
+
+      if (!passwordForgottenResponse.ok) {
+        addToast({
+          title: "Une erreur est survenue. Veuillez réessayer plus tard.",
+          color: "danger"
+        })
+        const { error } = await getJwtClaimsResponse.json()
+        console.error(error)
+        return
+      }
+
+      location.replace("/login")
+    } catch (err: any) {
+      console.error(err)
+      addToast({
+        title: "Une erreur est survenue. Veuillez réessayer plus tard.",
+        color: "danger"
+      })
+    }
+  }
+
+  return (
+    <div>
+      <h1 className={title()}>Mot de passe oublié</h1>
+      <p>Veuillez entrer l'adresse email lié à votre compte afin de recevoir un lien pour réinitialiser votre mot de passe.</p>
+      <Form encType="multipart/form-data" method="post" onSubmit={onSubmit}>
+        <Input
+          isRequired
+          errorMessage={getEmailError(email)}
+          isInvalid={getEmailError(email) !== null}
+          label="Adresse email"
+          labelPlacement="outside"
+          name="email"
+          placeholder="Entrez votre adresse email"
+          type="email"
+          value={email}
+          onValueChange={setEmail}
+        />
+        <Button color="primary" type="submit">
+          Confirmer
+        </Button>
+      </Form>
+    </div>
+  )
+}
