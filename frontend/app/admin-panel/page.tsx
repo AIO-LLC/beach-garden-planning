eaf3d17f3579b1a7416cb0c9faa7fef255f3f388
opixelum <48217573+opixelum@users.noreply.github.com>
2025-08-17 16:14:43 -0400

    feat(frontend): add modal to add member in admin panel

---
+++ frontend/app/admin-panel/page.tsx
@@ -0,0 +1,111 @@
+"use client"
+
+import { useState } from "react"
+import { button as buttonStyles } from "@heroui/theme"
+import { Form, Input, Button, addToast, useDisclosure } from "@heroui/react"
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
+} from "@heroui/modal"
+
+const API_HOST = process.env.NEXT_PUBLIC_API_HOST!
+const API_PORT = process.env.NEXT_PUBLIC_API_PORT!
+
+export default function AdminPanelPage() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure()
+
+  const [formState, setFormState] = useState({
+    phone: "",
+    phoneError: "",
+    isPhoneInvalid: false
+  })
+
+  const getPhoneError = (value: string): string => {
+    if (value.length === 0) return "Veuillez entrer un numéro de téléphone."
+    return ""
+  }
+
+  const onSubmit = onCloseFn => {
+    const phoneError = getPhoneError(formState.phone)
+    const isPhoneInvalid = phoneError !== ""
+
+    setFormState(prev => ({
+      ...prev,
+      phoneError,
+      isPhoneInvalid
+    }))
+
+    if (!isPhoneInvalid) {
+      onCloseFn()
+      setFormState(prev => ({
+        phone: "",
+        phoneError: "",
+        isPhoneInvalid: false
+      }))
+    }
+  }
+
+  return (
+    <>
+      <h1 className="font-bold text-xl my-4">Administration</h1>
+      <Button color="primary" onClick={onOpen}>
+        Ajouter un membre
+      </Button>
+      <Modal
+        isDismissable={false}
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        placement="center"
+      >
+        <ModalContent>
+          {onClose => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Ajouter un membre
+              </ModalHeader>
+              <Form
+                encType="multipart/form-data"
+                method="post"
+                onSubmit={event => {
+                  event.preventDefault()
+                  onSubmit(onClose)
+                }}
+              >
+                <ModalBody className="w-full">
+                  <Input
+                    className="w-full"
+                    errorMessage={formState.phoneError}
+                    isInvalid={formState.isPhoneInvalid}
+                    label="Numéro de téléphone"
+                    labelPlacement="outside"
+                    name="phone"
+                    placeholder="Entrez le numéro de téléphone"
+                    type="text"
+                    startContent="+"
+                    value={formState.phone}
+                    onValueChange={newValue => {
+                      setFormState(prev => ({
+                        phone: newValue,
+                        phoneError: "",
+                        isPhoneInvalid: false
+                      }))
+                    }}
+                  />
+                </ModalBody>
+                <ModalFooter className="w-full">
+                  <Button onPress={onClose}>Retour</Button>
+                  <Button color="primary" type="submit">
+                    Ajouter
+                  </Button>
+                </ModalFooter>
+              </Form>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
