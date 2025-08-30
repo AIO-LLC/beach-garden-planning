8599d7594b13c30d4a7212f8f331fbefb6eef4eb
opixelum <48217573+opixelum@users.noreply.github.com>
2025-08-30 02:13:29 +0200

    feat(backend): add aws lambda feature, tls & ssl crates

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,25 +1,41 @@
-use axum::{Error, Router};
 use backend::api::app::{AppState, build_state, router};
-use dotenvy::dotenv;
-use std::env;
-use std::net::SocketAddr;
+use lambda_http::Error;
+
+#[cfg(feature = "local")]
+use {
+    dotenvy::dotenv,
+    std::{env, net::SocketAddr},
+};
+
+#[cfg(not(feature = "local"))]
+use lambda_http::run;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    dotenv().ok();
+    #[cfg(feature = "local")]
+    {
+        dotenv().ok();
 
-    let api_ip: &str = &env::var("API_IP").expect("Undefined API_IP environment variable");
-    let api_port: u16 =
-        str::parse(&env::var("API_PORT").expect("Undefined API_PORT environment variable"))
-            .unwrap();
+        let api_ip: &str = &env::var("API_IP").expect("Undefined API_IP environment variable");
+        let api_port: u16 =
+            str::parse(&env::var("API_PORT").expect("Undefined API_PORT environment variable"))
+                .unwrap();
 
-    let app_state: AppState = build_state().await;
-    let app: Router = router(app_state).await;
+        let app_state: AppState = build_state().await;
+        let app = router(app_state).await;
+        let addr: SocketAddr = format!("{api_ip}:{api_port}").parse().expect("");
+        let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+        println!("Listening on {addr}");
 
-    let addr: SocketAddr = format!("{api_ip}:{api_port}").parse().expect("");
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    println!("Listening on {addr}");
+        axum::serve(listener, app).await.unwrap();
+    }
 
-    axum::serve(listener, app).await.unwrap();
+    #[cfg(not(feature = "local"))]
+    {
+        let app_state: AppState = build_state().await;
+        let app = router(app_state).await;
+        run(app).await?;
+    }
+
     Ok(())
 }
